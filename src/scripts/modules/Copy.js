a75b4959e0d38c445a76177b62b25f6fc362e2af
19h47 <jeremylevron@19h47.fr>
2022-02-21 13:22:24 +0100

    :sparkles: CopyButton

---
+++ src/scripts/modules/Copy.js
@@ -0,0 +1,44 @@
+import { module as M } from 'modujs';
+
+class Copy extends M {
+	constructor(m) {
+		super(m);
+
+		this.events = {
+			click: 'copy',
+		};
+	}
+
+	async copy() {
+
+		if (this.copyTimeout !== undefined) {
+			clearTimeout(this.copyTimeout);
+		}
+
+		this.copyTimeout = setTimeout(() => {
+			this.$('label')[0].innerText = this.getData('label');
+		}, 1500);
+
+		const item = this.getData('item');
+		const { state } = await navigator.permissions.query({ name: 'clipboard-write' })
+
+		if ('granted' === state || 'prompt' === state) {
+			navigator.clipboard.writeText(item).then(
+				() => {
+					this.$('label')[0].innerText = this.getData('success');
+				},
+				() => {
+					this.$('label')[0].innerText = this.getData('error');
+				},
+			);
+		} else {
+			this.$('label')[0].innerText = this.getData('error');
+		}
+	}
+
+	destroy() {
+		super.destroy();
+	}
+}
+
+export default Copy;
