9e5ee4e93f5e7bc7edff44909cd3753a6769083f
19h47 <jeremylevron@19h47.fr>
2022-02-22 08:54:22 +0100

    :sparkles: Menu animation

--- src/scripts/modules/NavItem.js
+++ src/scripts/modules/NavItem.js
@@ -4,11 +4,19 @@ class NavItem extends M {
 	open() {
 		this.el.classList.remove('opacity-0', 'invisible');
 		this.el.classList.add('opacity-100', 'visible');
+
+		this.$('item').forEach($tag => {
+			$tag.classList.add('is-inview');
+		});
 	}
 
 	close() {
 		this.el.classList.remove('opacity-100', 'visible');
 		this.el.classList.add('opacity-0', 'invisible');
+
+		this.$('item').forEach($tag => {
+			$tag.classList.remove('is-inview');
+		});
 	}
 }
 
